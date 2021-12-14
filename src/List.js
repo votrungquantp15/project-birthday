e7dfa98bfd44b6f48c482ca29e57b2f5da1f6967
Quan Vo <votrungquan.tp15@gmail.com>
2021-12-14 10:38:49 +0700

    birthday

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./List.css";
+
+const List = ({ people }) => {
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: "Quỳnh",
+    age: "20",
+    image:
+      "https://scontent.fsgn5-10.fna.fbcdn.net/v/t39.30808-6/p843x403/262119237_1017523115477237_5505767401950606587_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=rXm_4Lq9EAcAX8vhfbK&_nc_ht=scontent.fsgn5-10.fna&oh=00_AT-WPDrtMhr5DSL9BouXjuNVlxjMDco2dBHCBqhLtABY2A&oe=61BCFEE7",
+  },
+  {
+    id: 2,
+    name: "Quân",
+    age: "20",
+    image:
+      "https://scontent.fsgn5-8.fna.fbcdn.net/v/t1.6435-9/51732414_2057908061168337_6044470735957655552_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=O3FxFKPAsekAX-07mPd&_nc_ht=scontent.fsgn5-8.fna&oh=00_AT8C-dnzibWPKnu76QTIkdBcAIY_TLN0BwlPRvxCxmoHLw&oe=61DC466F",
+  },
+  {
+    id: 3,
+    name: "Vy",
+    age: "12",
+    image:
+      "https://scontent.fsgn5-8.fna.fbcdn.net/v/t1.6435-9/51732414_2057908061168337_6044470735957655552_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=O3FxFKPAsekAX-07mPd&_nc_ht=scontent.fsgn5-8.fna&oh=00_AT8C-dnzibWPKnu76QTIkdBcAIY_TLN0BwlPRvxCxmoHLw&oe=61DC466F",
+  },
+];
